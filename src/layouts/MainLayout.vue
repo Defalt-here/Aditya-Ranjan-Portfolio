01057fe88d4252af87daeb4ecd458dc0101783ea
Aditya Ranjan <adityaranjan_2004@outlook.com>
2024-03-07 02:32:40 +0530

    home page

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="home">
     <div class="container">
-      <h1 style="font-size: 90px" class="typingEffect_h">Hello I am</h1>
+      <h1 style="font-size: 90px">Hello I am</h1>
       <h1 style="font-size: 90px" class="typingEffect_name">
         <mark>Aditya Ranjan</mark>
       </h1>
-      <p class="typingEffect_p">(Yes, that's me!)</p>
+      <p id="p1">(Yes, that's me!)</p>
     </div>
   </div>
+  <div class="image"></div>
   <div class="buttonGroup q-gutter-lg">
-    <button class="btn">Lorem</button>
-    <button class="btn">Lorem</button>
-    <button class="btn">Lorem</button>
+    <router-link class="nav-link" to="/blog"
+      ><button class="btn">Games</button></router-link
+    >
+    <router-link class="nav-link" to="/blog"
+      ><button class="btn">Contact me</button></router-link
+    >
+    <a
+      href="https://drive.google.com/file/d/1zOx6AVj9h2pDZY2nJr4AHHpcfNIHRbuJ/view?usp=sharing"
+      target="_blank"
+    >
+      <button class="btn">Resumé</button>
+    </a>
   </div>
 </template>
 <script></script>
@@ -38,26 +48,20 @@ h1 {
 .buttonGroup {
   position: absolute;
   top: 78%;
-  left: 21%;
+  left: 23%;
   transform: translate(-50%, -50%);
   padding-top: 10px;
 }
 .btn {
   background-color: #ff6663;
   border-radius: 50px;
+  border-color: black;
+  border-style: solid;
+  transition: all 0.2s ease;
 }
 .btn:hover {
   box-shadow: 5px 5px #ff6663;
   transition: all 0.2s ease;
-}
-.typingEffect_h {
-  width: 0;
-  overflow: hidden; /* Ensure the text is not visible until the typewriter effect*/
-  border-right: 2px solid rgb(0, 0, 0); /* The cursor*/
-  font-size: 16px;
-  white-space: nowrap; /* Keeps the text on a single line */
-  animation: typing 2s forwards;
-  animation: typing 2s steps(30) forwards, blink 1s infinite;
 }
 .typingEffect_name {
   width: 0;
@@ -67,20 +71,14 @@ h1 {
   white-space: nowrap; /* Keeps the text on a single line */
   animation: typing 2s forwards;
   animation: typing 2s steps(30) forwards, blink 1s infinite;
-  animation-delay: 2s;
-}
-.typingEffect_p {
-  width: 0;
-  overflow: hidden; /* Ensure the text is not visible until the typewriter effect*/
-  border-right: 2px solid rgb(0, 0, 0); /* The cursor*/
-  font-size: 16px;
-  white-space: nowrap; /* Keeps the text on a single line */
-  animation: typing 2s forwards;
-  animation: typing 2s steps(30) forwards, blink 1s infinite;
-  animation-delay: 4s;
 }
 mark {
   background-color: antiquewhite;
+  transition: all 0.2s ease;
+}
+mark:hover {
+  background-color: rgb(234, 255, 189);
+  transition: all 0.2s ease;
 }
 /* The typing animation */
 @keyframes typing {
